564f61381095c21c08de668e2219416e9fa4903a
Allan Robert Tan <argt@duck.com>
2023-05-24 21:04:11 +0800

    add footer and header jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer>
+      <p>
+        <a href="https://github.com/eillanrt/pomodoro-timer" target="_blank">
+          <i className="fab fa-github"></i>
+          @eillanrt
+        </a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Pomodoro Clock</h1>
+    </header>
+  )
+}
+
+export default Header
