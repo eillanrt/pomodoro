8ed1654635fff8f08473136fa235b5f56d6ff417
Allan Robert Tan <argt@duck.com>
2023-06-01 10:21:57 +0800

    add rel="norefferer attribute"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ function Footer() {
   return (
     <footer>
       <p>
-        <a href="https://github.com/eillanrt/pomodoro" target="_blank">
+        <a
+          href="https://github.com/eillanrt/pomodoro"
+          target="_blank"
+          rel="noreferrer"
+        >
           <i className="fab fa-github"></i>
           Source Code
         </a>
